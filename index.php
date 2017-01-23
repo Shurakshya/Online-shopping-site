c8ec8087da693dad7a6f704ceed6a76c31c0eae9
shurakshya kharel <kharel.shurakshya@gmail.com>
2017-01-23 16:10:27 +0200

    final project

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   <link href='http://fonts.googleapis.com/css?family=Raleway' rel='stylesheet' type='text/css'>
-  <link rel="stylesheet" type="text/css" media="all" href="webshop.css" />
+  <link rel="stylesheet" type="text/css" media="all" href="/Stylesheets/webshop.css" />
   <script
     src="http://maps.googleapis.com/maps/api/js">
 </script>
